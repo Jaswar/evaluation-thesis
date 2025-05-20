68fbb8443774d31f50b65ebec3d0a3c8379e52fb
Jan Warchocki <janwarchocki@gmail.com>
2025-05-21 01:39:51 +0200

    Measure blur using an approach from a paper

--- blur_analysis.py
+++ blur_analysis.py
@@ -7,6 +7,25 @@ from common import get_masks, psnr, ssim, lpips, get_paths_from_model, get_mask_
 import matplotlib.pyplot as plt
 import cv2 as cv
 import scipy
+import blur_detector
+from tqdm import tqdm
+
+
+def get_blurs(source_path, gt_path, mask_type):
+    masks = get_masks(source_path, mask_type)
+    test_masks = masks[3::4]
+    gts = [cv.imread(os.path.join(gt_path, f)) for f in sorted(os.listdir(gt_path))]
+    blur_scores = []
+    for gt, mask in tqdm(zip(gts, test_masks)):
+        gray = cv.cvtColor(gt, cv.COLOR_BGR2GRAY)
+        # laplacian = cv.Laplacian(gray, cv.CV_64F)
+        # blur_score = laplacian[mask == 1].var()
+        blur_map = blur_detector.detectBlur(gray, downsampling_factor=4, num_scales=4, scale_start=2, num_iterations_RF_filter=3, show_progress=False)
+        blur_map = 1 - blur_map  # originally, 1 represents sharp objects
+        blur_score = blur_map[mask == 1].mean()
+        blur_scores.append(blur_score)
+    return blur_scores
+
 
 def evaluate(source_path, gt_path, renders_path, mask_type):
     if not os.path.exists(gt_path) or not os.path.exists(renders_path):
@@ -25,14 +44,6 @@ def evaluate(source_path, gt_path, renders_path, mask_type):
         assert mask.shape[0] == gt.shape[0] == render.shape[0], f'Height mismatch: {mask.shape[0]} vs {gt.shape[0]} vs {render.shape[0]}'
         assert mask.shape[1] == gt.shape[1] == render.shape[1], f'Width mismatch: {mask.shape[1]} vs {gt.shape[1]} vs {render.shape[1]}'
         assert gt.shape[2] == render.shape[2] == 3, f'Channel mismatch: {gt.shape[2]} vs {render.shape[2]}'
-
-    blur_scores = []
-    for gt, mask in zip(gts, test_masks):
-        gt = (gt * 255).clip(0, 255).astype(np.uint8)
-        gray = cv.cvtColor(gt, cv.COLOR_BGR2GRAY)
-        laplacian = cv.Laplacian(gray, cv.CV_64F)
-        blur_score = laplacian[mask == 1].var()
-        blur_scores.append(blur_score)
     
     psnrs = []
     ssims = []
@@ -47,7 +58,7 @@ def evaluate(source_path, gt_path, renders_path, mask_type):
             ssims.append(ssim_)
         if lpips_ is not None:
             lpipss.append(lpips_)
-    return psnrs, ssims, lpipss, blur_scores
+    return psnrs, ssims, lpipss
 
 
 def normalize(arr, make_log=True):
@@ -56,16 +67,16 @@ def normalize(arr, make_log=True):
     return (arr - np.min(arr)) / (np.max(arr) - np.min(arr))
 
 def main():
-    # models = ['EgoGaussian', 'Deformable-3D-Gaussians', '4DGaussians', '4d-gaussian-splatting']
-    models = ['Deformable-3D-Gaussians']
+    models = ['EgoGaussian', 'Deformable-3D-Gaussians', '4DGaussians', '4d-gaussian-splatting']
 
     with open('settings.json', 'r') as f:
         settings = json.load(f)
 
+    scene_blurs = {}
     for model in models:
         all_blurs = []
         all_psnrs = []
-        for repetition in ['0']:# ['0', '1', '2']:
+        for repetition in ['0', '1', '2']:
             root_path = f'retrain_output/{model}/retrain/ego_exo/random_search'
             for setting in settings[::-1]:
                 scene = setting['take_name']
@@ -76,21 +87,22 @@ def main():
                 source_path, gt_path, renders_path = get_paths_from_model(model, root_path, 'camera-rgb', scene, repetition)
                 task_specific_mask = get_mask_type_based_on_data_type('static', scene_type)
 
-                psnrs, ssims, lpipss, blur_scores = evaluate(source_path, gt_path, renders_path, task_specific_mask)
+                if scene not in scene_blurs:
+                    scene_blurs[scene] = get_blurs(source_path, gt_path, task_specific_mask)
+                blur_scores = scene_blurs[scene]
+
+                psnrs, ssims, lpipss = evaluate(source_path, gt_path, renders_path, task_specific_mask)
                 psnrs = normalize(psnrs, make_log=False)
                 blur_scores = normalize(blur_scores, make_log=False)
-                plt.plot(psnrs, label='PSNR')
-                plt.plot(blur_scores, label='Blur')
-                plt.legend()
-                plt.show()
                 all_blurs.extend(blur_scores)
                 all_psnrs.extend(psnrs)
-    #     plt.scatter(all_blurs, all_psnrs, label=model)
-    # plt.legend()
-    # plt.xlabel('Blurriness')
-    # plt.ylabel('PSNR')
-    # plt.show()
-                
+        plt.scatter(all_blurs, all_psnrs, label=model)
+    plt.legend()
+    plt.xlabel('Blurriness')
+    plt.ylabel('PSNR')
+    plt.show()
+    
+    print(scipy.stats.pearsonr(all_blurs, all_psnrs))
 
 if __name__ == '__main__':
     main()
