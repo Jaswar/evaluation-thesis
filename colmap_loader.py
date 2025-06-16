0fc25ced39729d4382c645020df180768758c5d6
Jan Warchocki <janwarchocki@gmail.com>
2025-06-16 23:54:18 +0200

    Credit for authors of 3DGS

--- colmap_loader.py
+++ colmap_loader.py
@@ -1,13 +1,5 @@
-#
-# Copyright (C) 2023, Inria
-# GRAPHDECO research group, https://team.inria.fr/graphdeco
-# All rights reserved.
-#
-# This software is free for non-commercial, research and evaluation use 
-# under the terms of the LICENSE.md file.
-#
-# For inquiries contact  george.drettakis@inria.fr
-#
+# file from 3DGS: https://github.com/graphdeco-inria/gaussian-splatting
+# all credit goes to the original authors
 
 import numpy as np
 import collections
